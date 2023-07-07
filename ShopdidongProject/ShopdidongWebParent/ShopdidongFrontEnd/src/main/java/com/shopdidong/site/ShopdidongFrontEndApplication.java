1935559453148c27a2e9ca32b22d182080bccf96
vodinhluan <luandinhvo2002@gmail.com>
2023-07-07 23:10:08 +0700

    shopdidong

---
+++ ShopdidongProject/ShopdidongWebParent/ShopdidongFrontEnd/src/main/java/com/shopdidong/site/ShopdidongFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.shopdidong.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopdidongFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopdidongFrontEndApplication.class, args);
+	}
+
+}
