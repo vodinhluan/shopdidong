ac41f36a1c907e9a627070f41ca09a2495bc313f
vodinhluan <luandinhvo2002@gmail.com>
2023-07-12 17:57:21 +0700

    unit test p1

--- ShopdidongProject/ShopdidongWebParent/ShopdidongBackEnd/src/test/java/com/shopdidong/admin/user/RoleRepositoryTests.java
+++ ShopdidongProject/ShopdidongWebParent/ShopdidongBackEnd/src/test/java/com/shopdidong/admin/user/RoleRepositoryTests.java
@@ -7,15 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
 
 import com.shopdidong.common.entity.Role;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
 public class RoleRepositoryTests {
 	
-	@Autowired
 	private RoleRepository repo;
+	 
+	@Autowired
+	public RoleRepositoryTests(RoleRepository repo) {
+	     this.repo = repo;
+	}
 	
 	@Test
 	public void testCreateFirstRole()
