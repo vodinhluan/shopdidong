c6981d0660d039daeb4fa4ab9f33c40cc5f8edf5
vodinhluan <luandinhvo2002@gmail.com>
2023-07-25 11:03:07 +0700

    check file sent to the server

--- ShopdidongProject/ShopdidongWebParent/ShopdidongBackEnd/src/main/java/com/shopdidong/admin/user/UserController.java
+++ ShopdidongProject/ShopdidongWebParent/ShopdidongBackEnd/src/main/java/com/shopdidong/admin/user/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopdidong.common.entity.Role;
@@ -38,11 +40,14 @@ public class UserController {
 	}
 	
 	@PostMapping("/users/save")
- 	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+ 	public String saveUser(User user, RedirectAttributes redirectAttributes,
+ 			@RequestParam("image") MultipartFile multipartFile) {
  		System.out.println(user);
- 		service.save(user);
-
- 		redirectAttributes.addFlashAttribute("message", "Lưu thành công!");
+ 		System.out.println(multipartFile.getOriginalFilename());
+ 		
+// 		service.save(user);
+//
+// 		redirectAttributes.addFlashAttribute("message", "Lưu thành công!");
 
  		return "redirect:/users";
  	}
