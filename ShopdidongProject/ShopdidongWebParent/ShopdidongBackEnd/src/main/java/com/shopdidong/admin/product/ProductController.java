539cfeb2bb6f1f050988c3b20bd0594cdbf6db60
vodinhluan <luandinhvo2002@gmail.com>
2023-09-01 14:02:32 +0700

    Product Overview Form

--- ShopdidongProject/ShopdidongWebParent/ShopdidongBackEnd/src/main/java/com/shopdidong/admin/product/ProductController.java
+++ ShopdidongProject/ShopdidongWebParent/ShopdidongBackEnd/src/main/java/com/shopdidong/admin/product/ProductController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopdidong.admin.brand.BrandService;
+import com.shopdidong.common.entity.Brand;
 import com.shopdidong.common.entity.Product;
 
 @Controller
@@ -16,12 +18,30 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	
+	@Autowired
+	private BrandService brandService;
+	
 	@GetMapping("/products")
 	public String listAll(Model model) {
 		List<Product> listProducts = productService.listAll();
 		model.addAttribute("listProducts", listProducts);
 		return "products/products";
 	}
+	
+	@GetMapping("/products/new")
+	public String newProduct(Model model) {
+		List<Brand> listBrands = brandService.listAll();
+		Product product = new Product();
+		product.setEnabled(true);
+		product.setInStock(true);
+		
+		model.addAttribute("product", product);
+		model.addAttribute("listBrands", listBrands);
+		model.addAttribute("pageTitle", "Create New Product");
+
+		return "products/product_form";
+	}
+	
 	
 	
 }
